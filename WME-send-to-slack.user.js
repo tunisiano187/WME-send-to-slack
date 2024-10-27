273f2597483e8c5390c9989a9f1885c2d203b431
Uwe4Waze <uwe4waze@fw51.de>
2024-10-27 22:46:48 +0100

    fix missing validation icon

--- WME-send-to-slack.user.js
+++ WME-send-to-slack.user.js
@@ -143,68 +143,65 @@ function init(e) {
     }
 
     // Check for changes in the edit-panel
-    const editPanelObserver = new MutationObserver(function(mutations) {
+    const editPanelObserver = new MutationObserver(function (mutations) {
         mutations.forEach((mutation) => {
-            // Mutation is a NodeList and doesn't support forEach like an array
-            for (let i = 0; i < mutation.addedNodes.length; i++) {
-                const addedNode = mutation.addedNodes[i];
-
+            const nodeList = mutation.addedNodes;
+            nodeList.forEach(addedNode => {
                 // Only fire up if it's a node
                 if (addedNode.nodeType === Node.ELEMENT_NODE) {
                     const lockLevelDiv = addedNode.querySelector('.lock-edit-view');
                     const closureListDiv = addedNode.querySelector('div.closures-list');
-                    const panel = addedNode.querySelector('#panel-container .edit-suggestion-panel');
+                    const panel = addedNode.querySelector('img[alt="suggester-level-icon"]');
 
                     if (lockLevelDiv) {
-                        sent=0;
+                        sent = 0;
                         log('Lock icons added');
                         $( "#WMESTSlock" ).remove();
                         $('.lock-edit-view').after('<div id="WMESTSlock">' + DOWNLOCK_ICON + '&nbsp;' + RE_LOCK_ICON + '</div>');
                         Loadactions();
                     }
                     if (closureListDiv) {
-                        sent=0;
+                        sent = 0;
                         log('Closure icons added');
-                        $( "#WMESTSclosures" ).remove();
+                        $("#WMESTSclosures").remove();
                         //Conditional no longer required...
                         $('.closures-list').before('<div id="WMESTSclosures">' + closureicon + '&nbsp;' + OPEN_ICON + '</div>');
                         $('.closures-list').height("auto");
-                        if(W.model.roadClosures.getObjectArray().length == 0) {
+                        if (W.model.roadClosures.getObjectArray().length == 0) {
                             $('.closures-list').height("auto");
                         }
                         Loadactions();
                     }
                     if (panel) {
-                        sent=0;
-                        appendValidationIcon();
+                        sent = 0;
+                        appendValidationIcon(addedNode);
                     }
                 }
             }
+            );
         });
     });
     editPanelObserver.observe(document.getElementById('edit-panel'), { childList: true, subtree: true });
 
     // Check for changes in panel section
-    const panelObserver = new MutationObserver(function(mutations) {
+    const panelObserver = new MutationObserver(function (mutations) {
         mutations.forEach((mutation) => {
-            // Mutation is a NodeList and doesn't support forEach like an array
-            for (let i = 0; i < mutation.addedNodes.length; i++) {
-                const addedNode = mutation.addedNodes[i];
-
+            const nodeList = mutation.addedNodes;
+            nodeList.forEach(addedNode => {
                 // Only fire up if it's a node
                 if (addedNode.nodeType === Node.ELEMENT_NODE) {
-                    const panel = addedNode.querySelector(' .edit-suggestion-panel');
+                    const panel = addedNode.querySelector('img[alt="suggester-level-icon"]');
                     if (panel) {
-                        sent=0;
-                        appendValidationIcon();
+                        sent = 0;
+                        appendValidationIcon(addedNode);
                     }
                 }
-            }
+            });
         });
     });
     panelObserver.observe(document.getElementById('panel-container'), { childList: true, subtree: true });
 
-    setTimeout(versionCheck,2000);
+    setTimeout(versionCheck, 2000);
 
     $('#WSTSFS-Container').css('display', 'block');
 }
@@ -500,7 +497,7 @@ function construct(iconAction) {
                     log("Editor Level check triggered, user decided to continue anyway.")
                     reason = AskReason();
                 }
-            } else { // always ask to get a chance to abort
+            } else { // ask always for reason to get a chance to abort
                 log("Editor Level checked, ask.")
                 reason = AskReason();
             }
@@ -896,6 +893,8 @@ function getPermalinkCleaned(iconaction) {
     let stateName = "";
     let selectedIndex = "";
     let selectionType = "&venues=";
+    let selectedIndex2 = ""; // used for Edit Suggestions
+    let selectionType2 = ""; // used for Edit Suggestions
     let requiredRank = 0;
     let shouldBeLockedAt = -5;
 
@@ -905,78 +904,110 @@ function getPermalinkCleaned(iconaction) {
     const mapCenter = new OpenLayers.Geometry.Point(center.lon, center.lat);
     const currentLocation = (new OpenLayers.LonLat(mapCenter.x, mapCenter.y)).transform(projI, projE).toString().replace(',', '&');
 
-    $.each(W.selectionManager.getSelectedWMEFeatures(), function(indx, section){
-        const data = section._wmeObject;
-        if (featureType === "venue") {
-            featureType = data.attributes.categories;
+    if (iconaction !== "Validation") {
+        $.each(W.selectionManager.getSelectedWMEFeatures(), function (indx, section) {
+            const data = section._wmeObject;
+            if (data.type === "venue") {
+                featureType = data.attributes.categories;
+            }
+            if (selectedIndex !== "") {
+                selectedIndex = selectedIndex + ",";
+            }
+            selectedIndex = selectedIndex + data.attributes.id;
+
+            if (data.type === "camera") {
+                selectionType = "&cameras=";
+                featureType = "camera";
+                featureTypeName = translationsInfo[24][0]
+
+            } else if (data.type === "bigJunction") {
+                selectionType = "&bigJunctions=";
+                featureType = "JB";
+                featureTypeName = translationsInfo[25][0]
+
+            } else if (data.type === "mapComment") {
+                selectionType = "&mapComments=";
+                featureType = "map comment";
+                featureTypeName = translationsInfo[26][0]
+
+            } else if (data.type === "railroadCrossing") {
+                selectionType = "&railroadCrossings=";
+                featureType = "Railroad Crossing";
+                featureTypeName = translationsInfo[27][0]
+
+            } else if (data.type === "restrictedDrivingArea") {
+                selectionType = "&restrictedDrivingAreas=";
+                featureType = "Restricted Area";
+                featureTypeName = translationsInfo[37][0];
+
+            } else if (data.type === 'segment') {
+                if (iconaction === "Validation") {
+                    //do nothing here except prepare for the count-up
+                    count--;
+                } else {
+                    selectionType = "&segments=";
+                    featureType = "segment";
+                    featureTypeName = translationsInfo[28][0];
+                    shouldBeLockedAt = getShouldLockedAt(data, shouldBeLockedAt);
+                }
+
+            } else {
+                log("unknown data type");
+            }
+            count++;
+            if (data.attributes.lockRank !== null && data.attributes.lockRank > requiredRank) {
+                requiredRank = data.attributes.lockRank;
+            } else if (data.attributes.rank > requiredRank) {
+                requiredRank = data.attributes.rank;
+            }
+            const cityId = getCityID(data, featureType);
+            cityName = getCity(cityId);
+            if (cityName == null) {
+                cityName = "";
+            }
+            countryName = getCountry(cityId);
+            stateName = getState(cityId);
+            log("State Name : " + stateName);
+            if (stateName === false) {
+                stateName = "";
+                log("State Name : emptyied" + stateName);
+            }
+        });
+        if (shouldBeLockedAt === -5) {
+            shouldBeLockedAt = "";
         }
-        if (selectedIndex !== "") {
-            selectedIndex=selectedIndex + ",";
+    } else { // Validation request
+        const suggestionContainer = document.querySelector('img[alt="suggester-level-icon"]').parentElement.parentElement.parentElement;
+        const suggestionSubHeader = suggestionContainer.querySelector('[class^="subHeader"]');
+        if (suggestionSubHeader.childElementCount >= 2) {
+            const suggestionID = suggestionSubHeader.childNodes[1].innerText.replace('ID: ', '');
+            const suggestions = W.selectionManager.model.editSuggestions.getObjectArray();
+            const suggestion = suggestions[0].attributes.suggestions[0];
+            const segmentID = suggestion.entityEdits[0].objectId;
+            selectionType = "&segments=";
+            selectedIndex = segmentID;
+            selectionType2 = "&editSuggestions=";
+            selectedIndex2 = suggestionID;
+            featureType = "editSuggestion";
+            featureTypeName = "Edit Suggestion";
+            const data = W.selectionManager.model.segments.getObjectById(segmentID);
+            //TODO Resolve doubled block
+            const cityId = getCityID(data, "segment");
+            cityName = getCity(cityId);
+            if (cityName == null) {
+                cityName = "";
+            }
+            countryName = getCountry(cityId);
+            stateName = getState(cityId);
+            log("State Name : " + stateName);
+            if (stateName === false) {
+                stateName = "";
+                log("State Name : emptyied" + stateName);
+            }
+            count++;
         }
-        selectedIndex = selectedIndex + data.attributes.id;
-
-        if (data.type === "camera") {
-            selectionType="&cameras=";
-            featureType="camera";
-            featureTypeName = translationsInfo[24][0]
-
-        } else if (data.type === "bigJunction") {
-            selectionType="&bigJunctions=";
-            featureType="JB";
-            featureTypeName = translationsInfo[25][0]
-
-        } else if (data.type === "mapComment") {
-            selectionType="&mapComments=";
-            featureType="map comment";
-            featureTypeName = translationsInfo[26][0]
-
-        } else if (data.type === "railroadCrossing") {
-            selectionType="&railroadCrossings=";
-            featureType="Railroad Crossing";
-            featureTypeName = translationsInfo[27][0]
-
-        } else if (data.type === "restrictedDrivingArea") {
-            selectionType="&restrictedDrivingAreas=";
-            featureType="Restricted Area";
-            featureTypeName = translationsInfo[37][0];
-
-        } else if (data.type !== 'venue') {
-            selectionType="&segments=";
-            featureType="segment";
-            featureTypeName = translationsInfo[28][0];
-            shouldBeLockedAt = getShouldLockedAt(data, shouldBeLockedAt);
-
-        }
-        count++;
-        if (data.attributes.lockRank !== null && data.attributes.lockRank > requiredRank) {
-            requiredRank = data.attributes.lockRank;
-        } else if (data.attributes.rank > requiredRank) {
-            requiredRank = data.attributes.rank;
-        }
-        const cityId = getCityID(data, featureType);
-        cityName = getCity(cityId);
-        if (cityName == null) {
-            cityName = "";
-        }
-        countryName = getCountry(cityId);
-        stateName = getState(cityId);
-        log("State Name : " + stateName);
-        if (stateName === false) {
-            stateName = "";
-            log("State Name : emptyied" + stateName);
-        }
-    });
-    if (shouldBeLockedAt === -5) {
-        shouldBeLockedAt = "";
     }
-    const suggestion = $(".edit-suggestion-panel .sub-header wz-caption");
-    if (suggestion.length >= 2) {
-        const segmentId = suggestion[1].innerText.replace('ID: ', '');
-        selectionType = "&editSuggestions=" + segmentId;
-        featureType="editSuggestion";
-        featureTypeName = "Edit Suggestion";
-    }
-    const PL = text + currentLocation + "&zoomLevel=" + W.map.getOLMap().getZoom() + selectionType + selectedIndex;
+    const PL = text + currentLocation + "&zoomLevel=" + W.map.getOLMap().getZoom() + selectionType + selectedIndex + selectionType2 + selectedIndex2;
     let type = featureTypeName;
     if (count > 1) {
         type = count + " " + type + translationsInfo[29][0];
@@ -1115,27 +1146,21 @@ function sendToDiscord(params, first, fallback) {
       })
 };
 
-function appendValidationIcon() {
-    let elem = document.querySelector(".edit-suggestion-panel");
+function appendValidationIcon(node) {
+    let elem = node?.querySelector('img[alt="suggester-level-icon"]');
     if (elem === null) {
-        setTimeout(appendValidationIcon, 100);
-        log("appendValidationIcon: edit-suggestion-panel is still missing; retrying");
+        setTimeout(appendValidationIcon(node), 100);
+        log("appendValidationIcon: the suggestion panel is still missing; retrying");
         return;
     }
-    elem = document.querySelector(".edit-suggestion-panel .panel-header-actions");
-    if (elem === null) {
-        setTimeout(appendValidationIcon, 100);
-        log("appendValidationIcon: header section is missing");
-        return; // Neither segment nor venue selected
-    }
-
-    const newDiv = document.createElement("div");
-    newDiv.id = "WMESTSvalidation";
-    newDiv.style = "margin: auto 10px";
-    newDiv.innerHTML = VALIDATION_ICON;
-
+    elem = node.querySelector('[class^="headerActions"]');
     if (elem) {
+        const newDiv = document.createElement("div");
+        newDiv.id = "WMESTSvalidation";
+        newDiv.style = "margin: auto 10px";
+        newDiv.innerHTML = VALIDATION_ICON;
         elem.appendChild(newDiv);
+        Loadactions();
         log('Validation icon added');
     }
 }
