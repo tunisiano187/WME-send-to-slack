425897601ebb4c4c5e4f8a760c6960174e8d904d
Uwe4Waze <uwe4waze@fw51.de>
2025-11-17 00:18:39 +0100

    Begin developing

--- WME-send-to-slack.user.js
+++ WME-send-to-slack.user.js
@@ -4,7 +4,7 @@
 // @namespace       https://wmests.bowlman.be
 // @description     Script to send Unlock/Closures/Validations requests to almost every Waze communities platform channels.
 // @description:fr  Ce script vous permettant d'envoyer vos demandes de délock/fermeture et de validation directement sur slack
-// @version         2024.11.27.01
+// @version         2024.11.27.01.dev
 // @downloadURL     https://update.greasyfork.org/scripts/408365/WME%20Send%20to%20Slack.user.js
 // @updateURL       https://update.greasyfork.org/scripts/408365/WME%20Send%20to%20Slack.user.js
 // @include 	    /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor.*$/
